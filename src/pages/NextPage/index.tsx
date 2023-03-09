6cb8643b13fa4a867ee825d9ae5272ba106fa691
SIH-Fortaleza <127309054+SIH-Fortaleza@users.noreply.github.com>
2023-03-09 11:59:10 -0300

    Ajuste de svg

--- src/pages/NextPage/index.tsx
+++ src/pages/NextPage/index.tsx
@@ -11,6 +11,7 @@ export default function NextPage(){
                     <img src="https://saocamilofortaleza.org.br/wp-content/uploads/2021/06/logo4.png" width={200}/>
                 </div>
                 <div className="svg">
+                    
                    <img id="svg" src="src/Images/health-professional-team-animate.svg" alt="svg" width={400}/>
                 </div>
                 <Agradecimento>
